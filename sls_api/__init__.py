0f90c005f0506b590d1cac383a7b39d520e9c5ad
HubbeKing <hubbe128@gmail.com>
2018-02-27 08:43:23 +0200

    Only import and register blueprints with configs
    This way, it's easier to install the API without having to set up all the endpoints and do a "partial" install

--- sls_api/__init__.py
+++ sls_api/__init__.py
@@ -1,13 +1,20 @@
 from __future__ import unicode_literals
 from flask import Flask
-from endpoints.digital_editions import digital_edition
-from endpoints.filemaker import filemaker
-from endpoints.oai import oai
-from endpoints.swift_accessfiles import swift
+import os
 app = Flask(__name__)
 
-# Register API endpoints here
-app.register_blueprint(digital_edition, url_prefix="/digitaledition")
-app.register_blueprint(filemaker, url_prefix="/filemaker")
-app.register_blueprint(oai, url_prefix="/oai")
-app.register_blueprint(swift, url_prefix="/accessfiles")
+# Selectively import and register endpoints based on which configs exist and can be loaded
+if os.path.exists(os.path.join("sls_api", "configs", "digital_editions.yml")):
+    from endpoints.digital_editions import digital_edition
+    app.register_blueprint(digital_edition, url_prefix="/digitaledition")
+if os.path.exists(os.path.join("sls_api", "configs", "filemaker.yml")):
+    from endpoints.filemaker import filemaker
+    app.register_blueprint(filemaker, url_prefix="/filemaker")
+if os.path.exists(os.path.join("sls_api", "configs", "oai.yml")):
+    from endpoints.oai import oai
+    app.register_blueprint(oai, url_prefix="/oai")
+if os.path.exists(os.path.join("sls_api", "configs", "swift_auth.yml")):
+    from endpoints.swift_accessfiles import swift
+    app.register_blueprint(swift, url_prefix="/accessfiles")
+
+print(" * Loaded endpoints: {}".format(", ".join(app.blueprints)))
