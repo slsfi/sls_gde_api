1c637f8e20b4715f2fbf41dff37e5785766fd58f
Rasmus Ek <rasmus.ek@sls.fi>
2018-12-18 15:54:00 +0200

    Add comments to publisher script

--- sls_api/scripts/publisher.py
+++ sls_api/scripts/publisher.py
@@ -1,4 +1,3 @@
-from lxml import etree
 import os
 from shutil import copy2 as copy_file
 from sqlalchemy import create_engine
@@ -86,15 +85,17 @@ def check_publication_mtimes_and_publish_files(project):
 
             connection.close()
 
+            # Keep a list of changed files for later git commit
             changes = []
+            # For each publication belonging to this project, check the modification timestamp of its master files and compare them to the generated web XML files
             for row in publication_info:
                 est_target_filename = "{}_{}_est.xml".format(row["publication.id"],
                                                              row["publication.publication_collection_id"])
                 com_target_filename = est_target_filename.replace("_est.xml", "_com.xml")
                 est_target_file_path = os.path.join(file_root, "xml", "est", est_target_filename)
                 com_target_file_path = os.path.join(file_root, "xml", "com", com_target_filename)
-                est_source_file_path = os.path.join(file_root, row["publication.original_filename"])
-                com_source_file_path = os.path.join(file_root, row["publication_comment.original_filename"])
+                est_source_file_path = os.path.join(file_root, row["publication.original_filename"])          # original_filename should be relative to the project root
+                com_source_file_path = os.path.join(file_root, row["publication_comment.original_filename"])  # original_filename should be relative to the project root
 
                 try:
                     est_target_mtime = os.path.getmtime(est_target_file_path)
@@ -102,6 +103,8 @@ def check_publication_mtimes_and_publish_files(project):
                     est_source_mtime = os.path.getmtime(est_source_file_path)
                     com_source_mtime = os.path.getmtime(com_source_file_path)
                 except OSError:
+                    # If there is an error, the web XML files likely don't exist or are otherwise corrupt
+                    # It is then easiest to just generate new ones
                     print("Error getting time_modified for target or source files for publication {}".format(row["publication.id"]))
                     print("Generating new est/com files...")
                     changes.append(est_target_file_path)
@@ -110,56 +113,66 @@ def check_publication_mtimes_and_publish_files(project):
                                                est_target_file_path, com_target_file_path)
                 else:
                     if est_target_mtime >= est_source_mtime and com_target_mtime >= com_source_mtime:
+                        # If both the est and com files are newer than the source files, just continue to the next publication
                         continue
                     else:
+                        # If one or either is outdated, generate new ones
                         changes.append(est_target_file_path)
                         changes.append(com_target_file_path)
                         print("Reading files for publication {} are outdated, generating new est/com files...".format(row["publication.id"]))
                         generate_est_and_com_files(project_name, est_source_file_path, com_source_file_path,
                                                    est_target_file_path, com_target_file_path)
 
+            # For each publication_version belonging to this project, check the modification timestamp of its master file and compare it to the generated web XML file
             for row in variant_info:
                 target_filename = "{}_{}_var_{}.xml".format(row["publication.publication_collection_id"],
                                                             row["publication.id"],
                                                             row["publication_version.id"])
                 source_filename = row["publication_version.original_filename"]
                 target_file_path = os.path.join(file_root, "xml", "var", target_filename)
-                source_file_path = os.path.join(file_root, source_filename)
+                source_file_path = os.path.join(file_root, source_filename)  # original_filename should be relative to the project root
 
                 try:
                     target_mtime = os.path.getmtime(target_file_path)
                     source_mtime = os.path.getmtime(source_file_path)
                 except OSError:
+                    # If there is an error, the web XML file likely doesn't exist or is otherwise corrupt
+                    # It is then easiest to just generate a new one
                     print("Error getting time_modified for target or source files for publication_version {}".format(row["publication_version.id"]))
                     print("Generating new file...")
                     changes.append(target_file_path)
                     generate_var_file(project_name, source_file_path, target_file_path)
                 else:
                     if target_mtime >= source_mtime:
+                        # If the target var file is newer than the source, continue to the next publication_version
                         continue
                     else:
                         changes.append(target_file_path)
                         print("File {} is newer than source file {}, generating new file...".format(target_file_path, source_file_path))
                         generate_var_file(project_name, source_file_path, target_file_path)
 
+            # For each publication_manuscript belonging to this project, check the modification timestamp of its master file and compare it to the generated web XML file
             for row in manuscript_info:
                 target_filename = "{}_{}_ms_{}.xml".format(row["publication.publication_collection_id"],
                                                            row["publication.id"],
                                                            row["publication_manuscript.id"])
                 source_filename = row["publication_manuscript.original_filename"]
                 target_file_path = os.path.join(file_root, "xml", "ms", target_filename)
-                source_file_path = os.path.join(file_root, source_filename)
+                source_file_path = os.path.join(file_root, source_filename)  # original_filename should be relative to the project root
 
                 try:
                     target_mtime = os.path.getmtime(target_file_path)
                     source_mtime = os.path.getmtime(source_file_path)
                 except OSError:
+                    # If there is an error, the web XML file likely doesn't exist or is otherwise corrupt
+                    # It is then easiest to just generate a new one
                     print("Error getting time_modified for target or source file for publication_manuscript {}".format(row["publication_manuscript.id"]))
                     print("Generating new file...")
                     changes.append(target_file_path)
                     generate_ms_file(project_name, source_file_path, target_file_path)
                 else:
                     if target_mtime >= source_mtime:
+                        # If the target ms file is newer than the source, continue to the next publication_manuscript
                         continue
                     else:
                         changes.append(target_file_path)
@@ -167,9 +180,12 @@ def check_publication_mtimes_and_publish_files(project):
                         generate_ms_file(project_name, source_file_path, target_file_path)
 
             if len(changes) > 0:
+                # If there are changes, try to commit them to git
                 try:
                     for change in changes:
+                        # Each changed file should be added, as there may be other activity in the git repo we don't want to commit
                         run_git_command(project_name, ["add", change])
+                    # Using Publisher as the author with the is@sls.fi email as a contact point should be fine
                     run_git_command(project_name, ["commit", "--author=Publisher <is@sls.fi>", "-m", "Published new web files"])
                     run_git_command(project_name, ["push"])
                 except CalledProcessError:
@@ -178,6 +194,7 @@ def check_publication_mtimes_and_publish_files(project):
 
 
 if __name__ == "__main__":
+    # TODO argparse to let you publish a single project if needed - for testing and debugging
     # For each project with a valid entry in the config file, check modification times for publications and publish
     for project_name in config.keys():
         check_publication_mtimes_and_publish_files(project_name)
