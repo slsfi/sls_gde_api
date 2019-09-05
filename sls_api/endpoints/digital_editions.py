10472725073fbbd838d49a098eea9cc777320542
SLS\niklil <niklas.liljestrand@sls.fi>
2019-09-05 14:53:37 +0300

    updated function

--- sls_api/endpoints/digital_editions.py
+++ sls_api/endpoints/digital_editions.py
@@ -353,7 +353,8 @@ def get_project_publication_facsimile_relations(project):
     connection = db_engine.connect()
     project_id = get_project_id_from_name(project)
     sql = sqlalchemy.sql.text(
-        "SELECT pc.id as pc_id, p.id as p_id, pf.id as pf_id FROM publication_collection pc \
+        "SELECT pc.id as pc_id, p.id as p_id, pf.id as pf_id,\
+         pc.name as pc_name, p.name as p_name, pf.page_nr FROM publication_collection pc \
          JOIN publication p ON p.publication_collection_id=pc.id \
          JOIN publication_facsimile pf ON pf.publication_id = p.id \
          WHERE project_id=:p_id ORDER BY pc.id")
