a7e2a39d5af800aad9296f26f4bfaa603f55233e
SLS\niklil <niklas.liljestrand@sls.fi>
2019-10-28 09:11:56 +0200

    fixing comments in publisher

--- sls_api/scripts/CTeiDocument.py
+++ sls_api/scripts/CTeiDocument.py
@@ -739,7 +739,7 @@ class CTeiDocument:
             try:
                 parser = ET.XMLParser(recover=True)
                 soup = BeautifulSoup(sHtml, "html.parser")
-                xml_doc_in = ET.XML("<note>"+html.escape((str(soup))+"</note>", parser)
+                xml_doc_in = ET.XML("<note>"+html.escape(str(soup))+"</note>", parser)
                 xml_doc_out = transform(xml_doc_in)
                 result = ET.tostring(xml_doc_out, encoding='unicode')
             except Exception as e:
