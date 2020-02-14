3d13370f27c9d081be095cfb19a7d629425df8cc
Rasmus Ek <rasmus.ek@sls.fi>
2020-02-14 11:59:15 +0200

    Use sqlalchemy bindparams to avoid SQL injections

--- sls_api/endpoints/workregister.py
+++ sls_api/endpoints/workregister.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
 import logging
+from sqlalchemy.sql import text
 
 from sls_api.endpoints.generics import db_engine, get_project_id_from_name
 
@@ -14,14 +15,15 @@ def get_work_manifestations_for_project(project):
     logger.info("Getting results for /workregister/manifestations/")
     connection = db_engine.connect()
     project_id = get_project_id_from_name(project)
-    manifestation_sql = "SELECT w_m.* FROM work_manifestation w_m \
-                            JOIN event_connection e_c ON e_c.work_manifestation_id = w_m.id \
-                            JOIN subject s ON s.id = e_c.subject_id \
-                            JOIN event e ON e.id = e_c.event_id \
-                            JOIN work_reference w_r ON w_r.work_manifestation_id = w_m.id \
-                            WHERE w_r.project_id = {} \
-                            AND e_c.deleted = 0 AND e.deleted = 0 AND w_r.deleted = 0 AND w_m.deleted = 0 AND s.deleted = 0 \
-                            ORDER BY w_m.title".format(project_id)
+    manifestation_sql = "SELECT w_m.* FROM work_manifestation w_m " \
+                        "JOIN event_connection e_c ON e_c.work_manifestation_id = w_m.id " \
+                        "JOIN subject s ON s.id = e_c.subject_id " \
+                        "JOIN event e ON e.id = e_c.event_id " \
+                        "JOIN work_reference w_r ON w_r.work_manifestation_id = w_m.id " \
+                        "WHERE w_r.project_id = :proj_id " \
+                        "AND e_c.deleted = 0 AND e.deleted = 0 AND w_r.deleted = 0 AND w_m.deleted = 0 AND s.deleted = 0 " \
+                        "ORDER BY w_m.title"
+    manifestation_sql = text(manifestation_sql).bindparams(proj_id=project_id)
 
     manifestations = []
     result = connection.execute(manifestation_sql)
@@ -45,15 +47,17 @@ def get_work_manifestation_authors_for_project(project, manifestation_id):
         connection.close()
         return jsonify({"msg": "Desired manifestation_id not found in database."}), 404
 
-    authors_sql = "SELECT s.* FROM work_manifestation w_m \
-                            JOIN event_connection e_c ON e_c.work_manifestation_id = w_m.id \
-                            JOIN subject s ON s.id = e_c.subject_id \
-                            JOIN event e ON e.id = e_c.event_id \
-                            JOIN work_reference w_r ON w_r.work_manifestation_id = w_m.id \
-                            WHERE w_m.id = {} \
-                            AND e_c.deleted = 0 AND e.deleted = 0 AND w_r.deleted = 0 AND w_m.deleted = 0 AND s.deleted = 0 \
-                            AND w_r.project_id = {} \
-                            ORDER BY s.last_name, s.full_name".format(manifestation_id, project_id)
+    authors_sql = "SELECT s.* FROM work_manifestation w_m " \
+                  "JOIN event_connection e_c ON e_c.work_manifestation_id = w_m.id " \
+                  "JOIN subject s ON s.id = e_c.subject_id " \
+                  "JOIN event e ON e.id = e_c.event_id " \
+                  "JOIN work_reference w_r ON w_r.work_manifestation_id = w_m.id " \
+                  "WHERE w_m.id = :mani_id " \
+                  "AND e_c.deleted = 0 AND e.deleted = 0 AND w_r.deleted = 0 AND w_m.deleted = 0 AND s.deleted = 0 " \
+                  "AND w_r.project_id = :proj_id " \
+                  "ORDER BY s.last_name, s.full_name"
+
+    authors_sql = text(authors_sql).bindparams(mani_id=manifestation_id, proj_id=project_id)
 
     authors = []
     result = connection.execute(authors_sql)
@@ -77,15 +81,17 @@ def get_work_manifestation_occurrences_for_project(project, manifestation_id):
         connection.close()
         return jsonify({"msg": "Desired manifestation_id not found in database."}), 404
 
-    occurrences_sql = "SELECT p.* FROM work_manifestation w_m \
-                            JOIN event_connection e_c ON e_c.work_manifestation_id = w_m.id \
-                            JOIN event e ON e.id = e_c.event_id \
-                            JOIN event_occurrence e_o ON e.id = e_o.event_id \
-                            JOIN work_reference w_r ON w_r.work_manifestation_id = w_m.id \
-                            JOIN publication p ON p.id = e_o.publication_id \
-                            WHERE w_m.id = {} \
-                            AND e_c.deleted = 0 AND e.deleted = 0 AND w_r.deleted = 0 AND p.deleted = 0 AND w_m.deleted = 0 AND e_o.deleted = 0 \
-                            AND w_r.project_id = {}".format(manifestation_id, project_id)
+    occurrences_sql = "SELECT p.* FROM work_manifestation w_m " \
+                      "JOIN event_connection e_c ON e_c.work_manifestation_id = w_m.id " \
+                      "JOIN event e ON e.id = e_c.event_id " \
+                      "JOIN event_occurrence e_o ON e.id = e_o.event_id " \
+                      "JOIN work_reference w_r ON w_r.work_manifestation_id = w_m.id " \
+                      "JOIN publication p ON p.id = e_o.publication_id " \
+                      "WHERE w_m.id = :mani_id " \
+                      "AND e_c.deleted = 0 AND e.deleted = 0 AND w_r.deleted = 0 AND p.deleted = 0 AND w_m.deleted = 0 AND e_o.deleted = 0 " \
+                      "AND w_r.project_id = :proj_id"
+
+    occurrences_sql = text(occurrences_sql).bindparams(mani_id=manifestation_id, proj_id=project_id)
 
     occurrences = []
     result = connection.execute(occurrences_sql)
