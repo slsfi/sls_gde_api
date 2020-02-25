724a123a2e71af411b232f818fdcc94b923766e1
Rasmus Ek <rasmus.ek@sls.fi>
2020-02-25 15:11:29 +0200

    Dump version number of package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='sls_api',
     packages=['sls_api'],
-    version="1.0.3",
+    version="1.1.0",
     include_package_data=True,
     install_requires=[
         'argon2_cffi',
