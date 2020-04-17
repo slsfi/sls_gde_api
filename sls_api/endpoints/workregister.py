9f4702c5b51d567e10f1b61df89c2278caafdd7f
SLS\niklil <niklas.liljestrand@sls.fi>
2020-04-17 12:01:48 +0300

    fix: add new endpoints to workregister

--- sls_api/endpoints/workregister.py
+++ sls_api/endpoints/workregister.py
@@ -98,6 +98,40 @@ def get_work_manifestation_occurrences_for_project(project, manifestation_id):
     return jsonify(occurrences)
 
 
+@workregister.route("/<project>/workregister/work/project/occurrences/<work_id>")
+def get_work_occurrences_by_work(project, work_id):
+    logger.info("Getting results for /workregister/work/project/occurrences/<work_id>")
+    connection = db_engine.connect()
+    project_id = get_project_id_from_name(project)
+    # Only allow int
+    if str(work_id).isnumeric() is False:
+        connection.close()
+        return jsonify({"msg": "Desired work_id not found in database."}), 404
+
+    occurrences_sql = """SELECT p.* FROM work_manifestation w_m
+                      JOIN event_connection e_c ON e_c.work_manifestation_id = w_m.id
+                      JOIN event e ON e.id = e_c.event_id
+                      JOIN event_occurrence e_o ON e.id = e_o.event_id
+                      JOIN work_reference w_r ON w_r.work_manifestation_id = w_m.id
+                      JOIN publication p ON p.id = e_o.publication_id
+                      WHERE w_m.work_id = :work_id
+                      AND e_c.deleted = 0 AND e.deleted = 0 AND w_r.deleted = 0 AND p.deleted = 0 AND w_m.deleted = 0 AND e_o.deleted = 0
+                      AND w_r.project_id = :proj_id"""
+
+    occurrences_sql = text(occurrences_sql).bindparams(work_id=work_id, proj_id=project_id)
+
+    occurrences = []
+    result = connection.execute(occurrences_sql)
+    row = result.fetchone()
+    while row is not None:
+        occurrences.append(dict(row))
+        row = result.fetchone()
+
+    connection.close()
+
+    return jsonify(occurrences)
+
+
 @workregister.route("/<project>/workregister/publication/manifestations/<publication_id>")
 def get_work_manifestations_for_publication(project, publication_id):
     logger.info("Getting results for /workregister/publication/manifestations/<publication_id>")
@@ -151,6 +185,31 @@ def get_work_manifestations_by_author(project, author_id):
         "ORDER BY w_m.title"
 
     sql = text(sql).bindparams(author_id=author_id)
+
+    data = []
+    result = connection.execute(sql)
+    row = result.fetchone()
+    while row is not None:
+        data.append(dict(row))
+        row = result.fetchone()
+
+    connection.close()
+
+    return jsonify(data)
+
+
+@workregister.route("/<project>/workregister/manifestation/<manifestation_id>")
+def get_work_manifestation_metadata(project, manifestation_id):
+    logger.info("Getting results for /workregister/manifestation/<manifestation_id>")
+    connection = db_engine.connect()
+    # Only allow int
+    if str(manifestation_id).isnumeric() is False:
+        connection.close()
+        return jsonify({"msg": "Desired manifestation_id not found in database."}), 404
+
+    sql = """SELECT w_m.* FROM work_manifestation w_m WHERE w_m.id = :manifestation_id w_m.deleted = 0 ORDER BY w_m.title"""
+
+    sql = text(sql).bindparams(manifestation_id=manifestation_id)
 
     data = []
     result = connection.execute(sql)
