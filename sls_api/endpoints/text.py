03545a9bf8913e8badba41d774f6c4bc1d8bbfb8
Niklas Liljestrand <niklas.liljestrand@sls.fi>
2021-11-12 13:31:05 +0200

    Update text.py

--- sls_api/endpoints/text.py
+++ sls_api/endpoints/text.py
@@ -54,7 +54,7 @@ def get_introduction(project, collection_id, publication_id, lang="swe"):
             content = get_content(project, "inl", filename, xsl_file, None)
             data = {
                 "id": "{}_{}_inl".format(collection_id, publication_id),
-                "content": content.replace("id=", "data-id=")
+                "content": content.replace(" id=", " data-id=")
             }
             return jsonify(data), 200
         else:
@@ -84,7 +84,7 @@ def get_title(project, collection_id, publication_id, lang="swe"):
             content = get_content(project, "tit", filename, xsl_file, None)
             data = {
                 "id": "{}_{}_tit".format(collection_id, publication_id),
-                "content": content.replace("id=", "data-id=")
+                "content": content.replace(" id=", " data-id=")
             }
             return jsonify(data), 200
         else:
@@ -129,7 +129,7 @@ def get_reading_text(project, collection_id, publication_id, section_id=None):
             content = get_content(project, "est", filename, xsl_file, {"bookId": bookId})
         data = {
             "id": "{}_{}_est".format(collection_id, publication_id),
-            "content": content.replace("id=", "data-id=")
+            "content": content.replace(" id=", " data-id=")
         }
         return jsonify(data), 200
     else:
@@ -302,8 +302,8 @@ def get_manuscript(project, collection_id, publication_id, manuscript_id=None, s
                 filename = "{}.xml".format(manuscript["legacy_id"])
             else:
                 filename = "{}_{}_ms_{}.xml".format(collection_id, publication_id, manuscript["id"])
-            manuscript_info[index]["manuscript_changes"] = get_content(project, "ms", filename, "ms_changes.xsl", params).replace("id=", "data-id=")
-            manuscript_info[index]["manuscript_normalized"] = get_content(project, "ms", filename, "ms_normalized.xsl", params).replace("id=", "data-id=")
+            manuscript_info[index]["manuscript_changes"] = get_content(project, "ms", filename, "ms_changes.xsl", params).replace(" id=", " data-id=")
+            manuscript_info[index]["manuscript_normalized"] = get_content(project, "ms", filename, "ms_normalized.xsl", params).replace(" id=", " data-id=")
 
         data = {
             "id": "{}_{}".format(collection_id, publication_id),
