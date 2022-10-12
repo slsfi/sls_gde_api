abeef92f914135bdb946c9bccc4b672d6e7f3ffa
Rasmus Ek <rasmus.ek@sls.fi>
2022-10-12 14:08:39 +0300

    Update dependencies to fix vulnerabilities

--- setup.py
+++ setup.py
@@ -7,21 +7,21 @@ setup(
     include_package_data=True,
     install_requires=[
         'argon2_cffi~=21.3.0',
-        'beautifulsoup4~=4.10.0',
+        'beautifulsoup4~=4.11.1',
         'elasticsearch==7.13.4',
-        'flask~=2.0.2',
-        'flask-jwt-extended~=4.3.1',
+        'flask~=2.2.2',
+        'flask-jwt-extended~=4.4.4',
         'flask-sqlalchemy~=2.5.1',
         'flask-cors~=3.0.10',
-        'lxml~=4.7.1',
-        'mysqlclient~=2.1.0',
+        'lxml~=4.9.1',
+        'mysqlclient~=2.1.1',
         'passlib~=1.7.4',
-        'psycopg2-binary~=2.9.2',
+        'psycopg2-binary~=2.9.4',
         'raven[flask]~=6.10.0',
-        'ruamel.yaml~=0.17.19',
-        'requests~=2.26.0',
-        'sqlalchemy~=1.4.29',
-        'Pillow~=8.4.0',
-        'werkzeug~=2.0.2'
+        'ruamel.yaml~=0.17.21',
+        'requests~=2.28.1',
+        'sqlalchemy~=1.4.41',
+        'Pillow~=9.2.0',
+        'werkzeug~=2.2.2'
     ]
 )
