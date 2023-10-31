58a159a1a714ec98b598c4c1b30316168f38560c
Rasmus Ek <rasmus.ek@sls.fi>
2023-10-31 09:13:14 +0200

    Update Flask to 3.0.0 and other dependencies to latest patch

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name='sls_api',
     packages=['sls_api'],
-    version="1.3.1",
+    version="1.3.3",
     include_package_data=True,
     install_requires=[
         'argon2_cffi==23.1.0',
         'beautifulsoup4==4.12.2',
         'elasticsearch==7.13.4',
-        'flask==2.3.3',
+        'flask==3.0.0',
         'flask-jwt-extended==4.5.3',
         'flask-sqlalchemy==3.1.1',
         'flask-cors==4.0.0',
@@ -18,10 +18,10 @@ setup(
         'passlib==1.7.4',
         'psycopg2-binary==2.9.9',
         'raven[flask]==6.10.0',
-        'ruamel.yaml==0.17.35',
+        'ruamel.yaml==0.18.3',
         'requests==2.31.0',
-        'sqlalchemy==2.0.21',
-        'Pillow==10.0.1',
-        'werkzeug==2.3.7'
+        'sqlalchemy==2.0.22',
+        'Pillow==10.1.0',
+        'werkzeug==3.0.1'
     ]
 )
