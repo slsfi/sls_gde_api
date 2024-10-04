7af03c8c10ffd9850005b721ac911b07a0945e8e
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2024-10-04 16:04:49 +0300

    format SQL query to be more readable

--- sls_api/endpoints/tools/collections.py
+++ sls_api/endpoints/tools/collections.py
@@ -376,14 +376,38 @@ def list_publication_collections(project):
     project_id = get_project_id_from_name(project)
     connection = db_engine.connect()
     # collections = get_table("publication_collection")
-    statement = """ SELECT pc.id, pc.name as title, pc.published, pc.date_created, pc.date_modified, pc.date_published_externally, pc.legacy_id,
-        pc.project_id, pc.publication_collection_title_id, pc.publication_collection_introduction_id, pc.name,
-        pct.original_filename AS collection_title_filename, pci.original_filename AS collection_intro_filename,
-        pct.published AS collection_title_published, pci.published AS collection_intro_published
-        FROM publication_collection pc
-        LEFT JOIN publication_collection_title pct ON pct.id = pc.publication_collection_title_id
-        LEFT JOIN publication_collection_introduction pci ON pci.id = pc.publication_collection_introduction_id
-        WHERE pc.project_id=:project_id AND pc.published>=1 ORDER BY pc.id """
+    statement = """
+        SELECT 
+            pc.id, 
+            pc.name AS title, 
+            pc.published, 
+            pc.date_created, 
+            pc.date_modified, 
+            pc.date_published_externally, 
+            pc.legacy_id,
+            pc.project_id, 
+            pc.publication_collection_title_id, 
+            pc.publication_collection_introduction_id, 
+            pc.name,
+            pct.original_filename AS collection_title_filename, 
+            pci.original_filename AS collection_intro_filename,
+            pct.published AS collection_title_published, 
+            pci.published AS collection_intro_published
+        FROM 
+            publication_collection pc
+        LEFT JOIN 
+            publication_collection_title pct 
+            ON pct.id = pc.publication_collection_title_id
+        LEFT JOIN 
+            publication_collection_introduction pci 
+            ON pci.id = pc.publication_collection_introduction_id
+        WHERE 
+            pc.project_id = :project_id 
+            AND pc.published >= 1 
+        ORDER BY 
+            pc.id
+    """
+
     statement = text(statement).bindparams(project_id=int_or_none(project_id))
     # statement = select(collections).where(collections.c.project_id == int_or_none(project_id))
     rows = connection.execute(statement).fetchall()
