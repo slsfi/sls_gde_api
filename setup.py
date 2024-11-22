c3f7141e4e29d2183b4e82491b686d59cb5c9886
Rasmus Ek <rasmus.ek@sls.fi>
2024-11-22 11:59:15 +0200

    Bump package versions

--- setup.py
+++ setup.py
@@ -8,21 +8,21 @@ setup(
     install_requires=[
         'argon2-cffi==23.1.0',
         'beautifulsoup4==4.12.3',
-        'elasticsearch==7.17.9',
-        'flask==3.0.3',
-        'flask-jwt-extended==4.6.0',
+        'elasticsearch==7.17.12',
+        'flask==3.1.0',
+        'flask-jwt-extended==4.7.1',
         'flask-sqlalchemy==3.1.1',
         'flask-cors==5.0.0',
-        'lxml==5.2.2',
-        'mysqlclient==2.2.4',
+        'lxml==5.3.0',
+        'mysqlclient==2.2.6',
         'passlib==1.7.4',
-        'Pillow==10.3.0',
-        'psycopg2-binary==2.9.9',
+        'Pillow==11.0.0',
+        'psycopg2-binary==2.9.10',
         'raven[flask]==6.10.0',
         'ruamel.yaml==0.18.6',
-        'requests==2.32.2',
-        'sqlalchemy==2.0.30',
-        'werkzeug==3.0.6',
-        'uwsgi==2.0.25.1'
+        'requests==2.32.3',
+        'sqlalchemy==2.0.36',
+        'werkzeug==3.1.3',
+        'uwsgi==2.0.28'
     ]
 )
